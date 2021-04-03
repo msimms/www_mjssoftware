88a00126eb0f2522bdeb4a5f3d8cdbb3fe2199cc
Mike Simms <msimms01@gmail.com>
2021-04-03 10:53:18 -0400

    Removed dead code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,6 @@
 
   $(window).on('load', function() {
 
-    /* 
-   MixitUp
-   ========================================================================== */
-  $('#portfolio').mixItUp();
-
   /* 
    One Page Navigation & wow js
    ========================================================================== */
@@ -65,29 +60,6 @@
      delay: 10,
      time: 1000
     });
-
-
-    /* Back Top Link active
-    ========================================================*/
-      var offset = 200;
-      var duration = 500;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
-        } else {
-          $('.back-to-top').fadeOut(400);
-        }
-      });
-
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
-
-
 
   });      
 
